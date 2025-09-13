7df227de59dc3d78b260461e1fd0e53754faf6d2
Ann Maria Abraham <133693843+annaby10@users.noreply.github.com>
2025-09-13 23:47:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ import os
 st.set_page_config(page_title="Marketing & Business Dashboard", layout="wide")
 
 # ---------- Utility Safe Loader ----------
-def safe_read(path, source_name):
+#def safe_read(path, source_name):
     """Read CSV safely, return DataFrame or empty with warning"""
-    if os.path.exists(path):
-        try:
-            df = pd.read_csv(path)
-            st.success(f"✅ Loaded {source_name} ({df.shape[0]} rows)")
-            return df
-        except Exception as e:
-            st.error(f"⚠️ Could not load {source_name}: {e}")
-            return pd.DataFrame()
-    else:
-        st.warning(f"⚠️ File not found: {path}")
-        return pd.DataFrame()
+    #if os.path.exists(path):
+        #try:
+       #     df = pd.read_csv(path)
+      #      st.success(f"✅ Loaded {source_name} ({df.shape[0]} rows)")
+     #       return df
+    #    except Exception as e:
+   #         st.error(f"⚠️ Could not load {source_name}: {e}")
+  #          return pd.DataFrame()
+   # else:
+   #     st.warning(f"⚠️ File not found: {path}")
+   #     return pd.DataFrame()  
 
 # ---------- Normalizer for Marketing ----------
 def norm(df, source_name):
